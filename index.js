a78619aa40f5b44c1dfe06419dd872c157ebdff3
BAWORID <fazzafabianahmad@my.id>
2024-06-15 18:36:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+    Author: yandricr
+    API: https://gpti.baworzz.my.id/api/
+    Docs: https://gpti.baworzz.my.id/
+*/
+
+const { gpt, dalle, lexica, prodia, util } = require("./main.js").default;
+
+module.exports = {
+    gpt, dalle, lexica, prodia, util
+}
